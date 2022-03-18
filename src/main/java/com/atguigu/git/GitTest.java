ee37622ffa144a2afee4ef9a245d7837471db4c1
zhaowenjie <2583512410@qq.com>
2022-03-18 17:13:39 +0800

    push1 commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
+        System.out.println("push1 test!");
     }
 }
 
